3311ae197d07e9638b2873da2a1d152a1d6d033f
ngf93 <nabiullinaguzel93@gmail.com>
2021-11-19 18:43:04 +0300

    git push

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import BtnFav from '../utilities/BtnFav';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+// Import Swiper React components
+import { Navigation, Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
 
 export default function MainPage() {
     return (
@@ -83,65 +86,200 @@ export default function MainPage() {
                 <img src="../img/map.png" alt="Карта" className="w-100"/>
             </div>
 
+            
+
             <div className="container mb-5">
                 <h2>Срочная продажа</h2>
-                <div className="row row-cols-4">
-                    <div>
-                        <div className="card-mini">
-                            <img src="../img/img3.jpg" alt="фото" className="w-100"/>
-                            <div className="p-3">
-                                <div className="d-flex justify-content-between mb-3">
-                                    <div className="color-1 title-font fw-7 fs-11">1-к, квартира 52м2</div>
-                                    <div className="title-font black fw-7 fs-11">6 000 000 ₽</div>
+                <div className="position-relative">
+                    <Swiper className="swiper-4"
+                        modules={[Navigation, Pagination]}
+                        navigation
+                        pagination={{ clickable: true }}
+                        spaceBetween={4}
+                        slidesPerView={2}
+                        breakpoints={{
+                            576: {
+                                slidesPerView: 3,
+                                spaceBetween: 4,
+                            },
+                            768: {
+                                slidesPerView: 3,
+                                spaceBetween: 16,
+                            },
+                            1200: {
+                                slidesPerView: 4,
+                                spaceBetween: 16,
+                            }
+                        }}
+                    >
+                        <SwiperSlide>
+                            <div className="card-mini">
+                                <img src="../img/img3.jpg" alt="фото" className="w-100"/>
+                                <div className="p-3">
+                                    <div className="d-flex justify-content-between mb-3">
+                                        <div className="color-1 title-font fw-7 fs-11">1-к, квартира 52м2</div>
+                                        <div className="title-font black fw-7 fs-11">6 000 000 ₽</div>
+                                    </div>
+                                    <div className="d-flex align-items-start mb-3">
+                                        <img src="../img/icons/pin.svg" alt="адрес"/>
+                                        <div className="fs-09 ms-2">
+                                            <div className="mb-1">ЖК “Столичный”</div>
+                                            <div>Вахитовский район, ул. Четаева 32</div>
+                                        </div>
+                                    </div>
+                                    <div className="text mb-4">
+                                        Новый дом. Консьерж. Квартира после евро ремонта. Полы ламинат. Кондиционеры. Рядом школа и деский сад...
+                                    </div>
+                                    <div className="text-end color-2 fs-09 fw-3">Вчера в 21:00</div>
                                 </div>
-                                <div className="d-flex align-items-start mb-3">
-                                    <img src="../img/icons/pin.svg" alt="адрес"/>
-                                    <div className="fs-09 ms-2">
-                                        <div className="mb-1">ЖК “Столичный”</div>
-                                        <div>Вахитовский район, ул. Четаева 32</div>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className="card-mini">
+                                <img src="../img/img3.jpg" alt="фото" className="w-100"/>
+                                <div className="p-3">
+                                    <div className="d-flex justify-content-between mb-3">
+                                        <div className="color-1 title-font fw-7 fs-11">1-к, квартира 52м2</div>
+                                        <div className="title-font black fw-7 fs-11">6 000 000 ₽</div>
+                                    </div>
+                                    <div className="d-flex align-items-start mb-3">
+                                        <img src="../img/icons/pin.svg" alt="адрес"/>
+                                        <div className="fs-09 ms-2">
+                                            <div className="mb-1">ЖК “Столичный”</div>
+                                            <div>Вахитовский район, ул. Четаева 32</div>
+                                        </div>
+                                    </div>
+                                    <div className="text mb-4">
+                                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae, sit voluptates. A quam officiis nesciunt dolores eum quibusdam vitae dolorum, sit ut in eius amet facilis atque dignissimos exercitationem velit!
+                                    </div>
+                                    <div className="text-end color-2 fs-09 fw-3">Вчера в 21:00</div>
+                                </div>
+                                <div className="labels">
+                                    <div className="vip">
+                                        <img src="../img/icons/vip.svg" alt="VIP"/> 
+                                        <span>VIP</span>
+                                    </div>
+                                    <div className="hot">
+                                        <img src="../img/icons/hot.svg" alt="Hot"/> 
+                                        <span>Hot</span>
                                     </div>
                                 </div>
-                                <div className="text mb-4">
-                                    Новый дом. Консьерж. Квартира после евро ремонта. Полы ламинат. Кондиционеры. Рядом школа и деский сад...
-                                </div>
-                                <div className="text-end color-2 fs-09 fw-3">Вчера в 21:00</div>
+                                <BtnFav/>
                             </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div className="card-mini">
-                            <img src="../img/img3.jpg" alt="фото" className="w-100"/>
-                            <div className="p-3">
-                                <div className="d-flex justify-content-between mb-3">
-                                    <div className="color-1 title-font fw-7 fs-11">1-к, квартира 52м2</div>
-                                    <div className="title-font black fw-7 fs-11">6 000 000 ₽</div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className="card-mini">
+                                <img src="../img/img3.jpg" alt="фото" className="w-100"/>
+                                <div className="p-3">
+                                    <div className="d-flex justify-content-between mb-3">
+                                        <div className="color-1 title-font fw-7 fs-11">1-к, квартира 52м2</div>
+                                        <div className="title-font black fw-7 fs-11">6 000 000 ₽</div>
+                                    </div>
+                                    <div className="d-flex align-items-start mb-3">
+                                        <img src="../img/icons/pin.svg" alt="адрес"/>
+                                        <div className="fs-09 ms-2">
+                                            <div className="mb-1">ЖК “Столичный”</div>
+                                            <div>Вахитовский район, ул. Четаева 32</div>
+                                        </div>
+                                    </div>
+                                    <div className="text mb-4">
+                                        Новый дом. Консьерж. Квартира после евро ремонта. Полы ламинат. Кондиционеры. Рядом школа и деский сад...
+                                    </div>
+                                    <div className="text-end color-2 fs-09 fw-3">Вчера в 21:00</div>
                                 </div>
-                                <div className="d-flex align-items-start mb-3">
-                                    <img src="../img/icons/pin.svg" alt="адрес"/>
-                                    <div className="fs-09 ms-2">
-                                        <div className="mb-1">ЖК “Столичный”</div>
-                                        <div>Вахитовский район, ул. Четаева 32</div>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className="card-mini">
+                                <img src="../img/img3.jpg" alt="фото" className="w-100"/>
+                                <div className="p-3">
+                                    <div className="d-flex justify-content-between mb-3">
+                                        <div className="color-1 title-font fw-7 fs-11">1-к, квартира 52м2</div>
+                                        <div className="title-font black fw-7 fs-11">6 000 000 ₽</div>
+                                    </div>
+                                    <div className="d-flex align-items-start mb-3">
+                                        <img src="../img/icons/pin.svg" alt="адрес"/>
+                                        <div className="fs-09 ms-2">
+                                            <div className="mb-1">ЖК “Столичный”</div>
+                                            <div>Вахитовский район, ул. Четаева 32</div>
+                                        </div>
+                                    </div>
+                                    <div className="text mb-4">
+                                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae, sit voluptates. A quam officiis nesciunt dolores eum quibusdam vitae dolorum, sit ut in eius amet facilis atque dignissimos exercitationem velit!
+                                    </div>
+                                    <div className="text-end color-2 fs-09 fw-3">Вчера в 21:00</div>
+                                </div>
+                                <div className="labels">
+                                    <div className="vip">
+                                        <img src="../img/icons/vip.svg" alt="VIP"/> 
+                                        <span>VIP</span>
+                                    </div>
+                                    <div className="hot">
+                                        <img src="../img/icons/hot.svg" alt="Hot"/> 
+                                        <span>Hot</span>
                                     </div>
                                 </div>
-                                <div className="text mb-4">
-                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae, sit voluptates. A quam officiis nesciunt dolores eum quibusdam vitae dolorum, sit ut in eius amet facilis atque dignissimos exercitationem velit!
-                                </div>
-                                <div className="text-end color-2 fs-09 fw-3">Вчера в 21:00</div>
+                                <BtnFav/>
                             </div>
-                            <div className="labels">
-                                <div className="vip">
-                                    <img src="../img/icons/vip.svg" alt="VIP"/> 
-                                    <span>VIP</span>
-                                </div>
-                                <div className="hot">
-                                    <img src="../img/icons/hot.svg" alt="Hot"/> 
-                                    <span>Hot</span>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className="card-mini">
+                                <img src="../img/img3.jpg" alt="фото" className="w-100"/>
+                                <div className="p-3">
+                                    <div className="d-flex justify-content-between mb-3">
+                                        <div className="color-1 title-font fw-7 fs-11">1-к, квартира 52м2</div>
+                                        <div className="title-font black fw-7 fs-11">6 000 000 ₽</div>
+                                    </div>
+                                    <div className="d-flex align-items-start mb-3">
+                                        <img src="../img/icons/pin.svg" alt="адрес"/>
+                                        <div className="fs-09 ms-2">
+                                            <div className="mb-1">ЖК “Столичный”</div>
+                                            <div>Вахитовский район, ул. Четаева 32</div>
+                                        </div>
+                                    </div>
+                                    <div className="text mb-4">
+                                        Новый дом. Консьерж. Квартира после евро ремонта. Полы ламинат. Кондиционеры. Рядом школа и деский сад...
+                                    </div>
+                                    <div className="text-end color-2 fs-09 fw-3">Вчера в 21:00</div>
                                 </div>
                             </div>
-                            <BtnFav/>
-                        </div>
-                    </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className="card-mini">
+                                <img src="../img/img3.jpg" alt="фото" className="w-100"/>
+                                <div className="p-3">
+                                    <div className="d-flex justify-content-between mb-3">
+                                        <div className="color-1 title-font fw-7 fs-11">1-к, квартира 52м2</div>
+                                        <div className="title-font black fw-7 fs-11">6 000 000 ₽</div>
+                                    </div>
+                                    <div className="d-flex align-items-start mb-3">
+                                        <img src="../img/icons/pin.svg" alt="адрес"/>
+                                        <div className="fs-09 ms-2">
+                                            <div className="mb-1">ЖК “Столичный”</div>
+                                            <div>Вахитовский район, ул. Четаева 32</div>
+                                        </div>
+                                    </div>
+                                    <div className="text mb-4">
+                                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae, sit voluptates. A quam officiis nesciunt dolores eum quibusdam vitae dolorum, sit ut in eius amet facilis atque dignissimos exercitationem velit!
+                                    </div>
+                                    <div className="text-end color-2 fs-09 fw-3">Вчера в 21:00</div>
+                                </div>
+                                <div className="labels">
+                                    <div className="vip">
+                                        <img src="../img/icons/vip.svg" alt="VIP"/> 
+                                        <span>VIP</span>
+                                    </div>
+                                    <div className="hot">
+                                        <img src="../img/icons/hot.svg" alt="Hot"/> 
+                                        <span>Hot</span>
+                                    </div>
+                                </div>
+                                <BtnFav/>
+                            </div>
+                        </SwiperSlide>
+                    </Swiper>
                 </div>
+                
             </div>
         </div>
     )
