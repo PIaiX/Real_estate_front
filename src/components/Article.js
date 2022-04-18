1fc7a43cd1c0c155a41916962870af2bc89ccc1e
IzmaIs <ichestor0@gmail.com>
2022-04-18 10:32:20 +0300

    Fix: fixed the router bug

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
 
+
 export default function Article(props) {
     const goToTop = () => {
         window.scrollTo({
@@ -9,14 +10,16 @@ export default function Article(props) {
         });
     };
 
-
     return (
         <article >
             <img src={props.imgUrl} alt={props.title}/>
             <div className="py-2 py-md-3 px-3 px-md-4">
                 <h2 className="mb-2 mb-md-3">{props.title}</h2>
                 <div className="text">{props.text}</div>
-                <NavLink to={props.articleUrl} onClick={() => goToTop()} className="d-block text-center color-2 fs-11 fw-5 mt-1">Читать далее</NavLink>
+                <NavLink
+                    to={`/${props.articleUrl}`}
+                    onClick={() => goToTop()}
+                    className="d-block text-center color-2 fs-11 fw-5 mt-1">Читать далее</NavLink>
             </div>
         </article>
     )
