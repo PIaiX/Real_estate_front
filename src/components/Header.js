95569beb143507808a439386ae50b15174678ae0
ngf93 <nabiullinaguzel93@gmail.com>
2022-04-20 16:51:23 +0300

    Fix: ask modal window

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,7 +35,7 @@ export default function Header() {
                             </ul>
                         </div>
                         <a href="/">Ипотека</a>
-                        <a href="/">Задать вопрос</a>
+                        <a href="" role="button" data-bs-toggle="modal" data-bs-target="#ask">Задать вопрос</a>
                     </nav>
                     <div className="d-none d-md-flex order-4 order-lg-3">
                         <Link to="/personal-account/my-messages" onClick={() => scrollToTop()} className="ms-4">
@@ -79,6 +79,56 @@ export default function Header() {
                             <path d="M14.9999 1.18237L1.00001 15.9049"/>
                         </svg>
                     </button>
+                </div>
+            </div>
+
+            <div className="modal fade" id="ask" tabIndex="-1" aria-hidden="true">
+                <div className="modal-dialog modal-lg">
+                    <div className="modal-content">
+                        <div className="modal-body px-lg-5">
+                            <button type="button" className="btn-close" data-bs-dismiss="modal">
+                                <svg viewBox="0 0 16 17" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M1.00006 1.18237L15 15.9049"/>
+                                    <path d="M14.9999 1.18237L1.00001 15.9049"/>
+                                </svg>
+                            </button>
+                            <h3 className='text-center'>Задать вопрос</h3>
+                            <form className="message-form">
+                                <div className="d-flex align-items-center">
+                                    <div className="photo me-2 me-sm-4">
+                                        <img src="/real_estate/img/photo.png" alt="Колесникова Ирина"/>
+                                        <div className="indicator online"></div>
+                                    </div>
+                                    <div>
+                                        <div className='fs-11 fw-5'>Вам ответит администратор</div> 
+                                        <div className='fs-11 fw-5 mt-1'>Колесникова Ирина</div>
+                                        <div className="gray-2 fs-09 mt-2">Сейчас онлайн</div>
+                                    </div>
+                                </div>
+                                <div className='row align-items-center fs-11 mt-3'>
+                                    <div className='col-sm-3 mb-1 mb-sm-3'>
+                                        <label className='gray-3' for="name">Ваше имя:</label>
+                                    </div>
+                                    <div className='col-sm-9 mb-3'>
+                                        <input type="text" placeholder="Имя" id="name"/>
+                                    </div>
+                                    <div className='col-sm-3 mb-1 mb-sm-3'>
+                                        <label className='gray-3' for="email">Ваш Email:</label>
+                                    </div>
+                                    <div className='col-sm-9 mb-3'>
+                                        <input type="text" placeholder="Email" id="email"/>
+                                    </div>
+                                    <div className='col-sm-3 mb-1 mb-sm-3'>
+                                        <label className='gray-3' for="question">Ваш вопрос:</label>
+                                    </div>
+                                    <div className='col-sm-9 mb-sm-3'>
+                                        <input type="text" placeholder="вопрос" id="question"/>
+                                    </div>
+                                </div>
+                                <button type="submit" className="btn btn-1 mx-auto fs-12 mt-4">ОТПРАВИТЬ</button>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
