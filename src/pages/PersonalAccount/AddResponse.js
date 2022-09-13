ee52f1ea518ca7708fe5f56f53aa0953a60aaa70
Dmitry Gavrilov <gavrstyle@gmail.com>
2022-09-13 11:55:48 +0300

    Fix: fixed styles

--- src/pages/PersonalAccount/AddResponse.js
+++ src/pages/PersonalAccount/AddResponse.js
@@ -14,7 +14,6 @@ export default function AddResponse() {
     const token = useSelector(state => state?.accessToken)
     const userId = useSelector(state => state?.currentUser?.id)
     const [response, setResponse] = useState('')
-    const {setSubmitAlert, getAlertNode} = useAlert(3000)
     const [userInf, setUserInf] = useState({})
     const loc = useLocation()
     const navigate = useNavigate()
@@ -30,29 +29,33 @@ export default function AddResponse() {
 
         createResponse(axiosPrivate, token, payloads)
             .then(result => {
-                (result?.status === 200)
-                    ? setSubmitAlert(prev => ({
-                        ...prev,
-                        isShow: true,
-                        variant: 'success',
-                        message: 'Отклик успешно отправлен, отправляем вас назад'
-                    }))
-                    : setSubmitAlert(prev => ({
-                        ...prev,
-                        isShow: true,
-                        variant: 'danger',
-                        message: 'Что-то пошло не так, не удалось отправить отклик'
-                    }))
+                // ! needed dispatch alerts
+                // (result?.status === 200)
+
+                    // ? setSubmitAlert(prev => ({
+                    //     ...prev,
+                    //     isShow: true,
+                    //     variant: 'success',
+                    //     message: 'Отклик успешно отправлен, отправляем вас назад'
+                    // }))
+                    // : setSubmitAlert(prev => ({
+                    //     ...prev,
+                    //     isShow: true,
+                    //     variant: 'danger',
+                    //     message: 'Что-то пошло не так, не удалось отправить отклик'
+                    // }))
                 payloads = {}
                 setTimeout(() => {navigate(loc?.state?.prevUrl)}, 1500)
 
             })
-            .catch(() => setSubmitAlert(prev => ({
-                ...prev,
-                isShow: true,
-                variant: 'danger',
-                message: 'Возникла ошибка при создании отклика'
-            })))
+            .catch(() => null
+        // setSubmitAlert(prev => ({
+            //     ...prev,
+            //     isShow: true,
+            //     variant: 'danger',
+            //     message: 'Возникла ошибка при создании отклика'
+            // }))
+            )
     }
 
     useEffect(() => {
@@ -61,7 +64,6 @@ export default function AddResponse() {
 
     return (
         <div className='px-2 px-sm-4 pb-4 pb-sm-5'>
-            {getAlertNode()}
             <nav className="d-block d-lg-none mt-3 mb-3 mb-sm-5" aria-label="breadcrumb">
                 <Link to="/personal-account" className="gray-3">&#10094; Назад</Link>
             </nav>
