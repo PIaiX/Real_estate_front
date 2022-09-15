12457a0da92af0b7f214f9fd5394fa4101f8acf3
Dmitry Gavrilov <gavrstyle@gmail.com>
2022-09-15 18:40:16 +0300

    Refactor: added topic theme for conversations

--- src/components/ConversationItem.js
+++ src/components/ConversationItem.js
@@ -8,23 +8,29 @@ const ConversationItem = ({conversation}) => {
     const user = useSelector(state => state?.currentUser)
     const isMyLastMessage = (user?.id === conversation?.lastMessage?.userId)
 
+    console.log(conversation)
+
     return (
         <div className={`conversation-list__item conversation-item ${conversation?.isNew ? 'unread' : ''}`}>
             <div className="photo">
-                <img src="/img/photo5.png" alt="Баженов Илья"/>
+                <img src={checkPhotoPath(conversation?.user?.avatar)} alt={conversation?.user?.fullName || 'avatar'}/>
                 {/*<div className="indicator" />*/}
             </div>
             <div className="main">
                 <div className="title-font fs-11 fw-7 mb-1">{conversation?.user?.fullName || 'Не известный'}</div>
-                <div className="fs-09 fw-5 mb-3 mb-sm-4">Тема: 1-к, квартира 52м2, Четаева 32</div>
+                {(conversation?.realEstate?.title && conversation?.realEstate?.residentalComplex) && (
+                    <div className="fs-09 fw-5 mb-3 mb-sm-4">
+                        {`${conversation?.realEstate?.title} ${conversation?.realEstate?.residentalComplex}`}
+                    </div>
+                )}
                 <div className="reply">
                     {(isMyLastMessage) && <img src={checkPhotoPath(user?.avatar)} alt={user?.fullName}/>}
                     <div className="message">{conversation?.lastMessage?.text}</div>
                     {(isMyLastMessage) && (
                         <div className="mark">
                             {conversation?.lastMessage?.isViewed
-                                ? <i className="bi bi-check2-all" title="прочитано" />
-                                : <i className="bi bi-check2" title="Отправлено" />
+                                ? <i className="bi bi-check2-all" title="прочитано"/>
+                                : <i className="bi bi-check2" title="Отправлено"/>
                             }
                         </div>
                     )}
@@ -43,7 +49,7 @@ const ConversationItem = ({conversation}) => {
                 {/*    <i className="bi bi-exclamation-triangle-fill" />*/}
                 {/*</button>*/}
             </div>
-            <Link to={`chat/${conversation?.id}`} className="link-to-chat" />
+            <Link to={`chat/${conversation?.id}`} className="link-to-chat"/>
         </div>
     );
 };
